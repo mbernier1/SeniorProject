4f51bfb478cf3a50d9ac587d4c3f0af0ddf9c4a2
Mike <38372845+mbernier1@users.noreply.github.com>
2022-01-18 00:23:22 -0800

    Added buttons
    Added buttons for moving between activities

---
+++ app/src/main/java/com/example/learningwithfriends/ChooseGames23.java
@@ -0,0 +1,14 @@
+package com.example.learningwithfriends;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseGames23 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_games23);
+    }
+}
---
+++ app/src/main/java/com/example/learningwithfriends/ChooseGames45.java
@@ -0,0 +1,14 @@
+package com.example.learningwithfriends;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseGames45 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_games45);
+    }
+}
