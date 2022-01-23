ed481194669d79b469cc361b131cfe108ed9533d
Mike <38372845+mbernier1@users.noreply.github.com>
2022-01-23 11:55:44 -0800

    added icons
    Added a lot of icons for different games: numbers, letters and colors

--- app/src/main/java/com/example/learningwithfriends/LettersGameLogic.java
+++ app/src/main/java/com/example/learningwithfriends/LettersGameLogic.java
@@ -1,10 +1,24 @@
 package com.example.learningwithfriends;
 
+import java.util.*;
+import java.util.Hashtable;
+import java.util.Map;
+import java.lang.Object;
+import java.util.Dictionary;
+import java.util.Hashtable;
 public class LettersGameLogic {
 
     private int choice = 0;
+    final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // convert the string to a char array
+    char[] alphabetArr = alphabet.toCharArray();
 
     //make table of letter/icon pairs
+
+    //Hashtable<String, Object> letter_icons = new Hashtable<String, Object> ();
+    //letter_icons.put("A", 23);
+
+
     //randomaly pull a letter/icon
     //  display icon
     //  announce letter
