38b965c087db9d34dac3f43da7429461e4d780a2
HumaneOrpheus12 <38372845+mbernier1@users.noreply.github.com>
2022-02-18 14:38:47 -0800

    added more shape game logic

--- app/src/main/java/com/example/learningwithfriends/ShapesGame.java
+++ app/src/main/java/com/example/learningwithfriends/ShapesGame.java
@@ -3,6 +3,10 @@ package com.example.learningwithfriends;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShapesGame extends AppCompatActivity {
 
@@ -11,10 +15,27 @@ public class ShapesGame extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shapes_game);
 
+        ImageView imageView1 = findViewById(R.id.shape_image_1);
+        ImageView imageView2 = findViewById(R.id.shape_image_2);
+        ImageView imageView3 = findViewById(R.id.shape_image_3);
+        ImageView imageView4 = findViewById(R.id.shape_image_4);
+        ImageView imageView5 = findViewById(R.id.shape_image_5);
+        ImageView imageView6 = findViewById(R.id.shape_image_6);
+
 
         LettersGameLogic startGame = new LettersGameLogic();
 
         //Object list of letters in random order
         Object[] randomizedShapes = startGame.RandomKeyGenerator().toArray();
+
+        //List<Integer> randomShapes = new ArrayList<>(ShapesGameLogic.shapes.values());
+
+
+        imageView1.setImageResource((Integer) randomizedShapes[0]);// .get(0));
+        imageView2.setImageResource((Integer) randomizedShapes[1]);//.get(1));
+        imageView3.setImageResource((Integer) randomizedShapes[2]);//.get(2));
+        imageView4.setImageResource((Integer) randomizedShapes[3]);//.get(3));
+        imageView5.setImageResource((Integer) randomizedShapes[4]);//.get(4));
+        imageView6.setImageResource((Integer) randomizedShapes[5]);//.get(5));
     }
 }
